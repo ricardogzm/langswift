3bb907c80c5c76cbbca11032f44c426b195bbd14
Ricardo Guzman <ricardogzm.dev@gmail.com>
2023-06-29 01:10:01 -0600

    feat: Update UI components to adopt new shadcn CLI
    - Update dependencies
    - Add new dependencies like openai-edge, ai, zod, env-nextjs for future use.
    - Also, revert to only use `next dev` without `--turbo`, as it breaks page routing when change reloading.

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,18 +1,32 @@
 import React from "react";
-
 import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const textareaVariants = cva(
+  "flex min-h-[5rem] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+        ringless: "",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+    VariantProps<typeof textareaVariants> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, variant, ...props }, ref) => {
     return (
       <textarea
-        className={cn(
-          "flex min-h-[5rem] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
+        className={cn(textareaVariants({ variant, className }))}
         ref={ref}
         {...props}
       />
