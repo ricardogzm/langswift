dba688f13669691b66dea8d7d71aee0d79a0cac4
Ricardo Guzman <ricardogzm.dev@gmail.com>
2023-09-12 21:03:27 -0600

    fix(seo): use metadataBase property according to environment

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,24 +2,29 @@ import { Metadata } from "next";
 
 type OpenGraph = Metadata["openGraph"];
 
+const PROJECT_ENV = process.env.VERCEL_ENV ?? "development";
+const PROJECT_URL = process.env.VERCEL_URL ?? "http://localhost:3000";
+
+const baseURL =
+  PROJECT_ENV === "production" ? "https://langswift.com" : PROJECT_URL;
+
 const openGraph: OpenGraph = {
   type: "website",
   siteName: "Langswift",
   images: [
     {
       url: "/og.png",
-      width: 1200,
-      height: 630,
       alt: "Langswift",
+      type: "image/png",
     },
   ],
 };
 
 export const siteConfig = {
+  url: baseURL,
   name: "Langswift",
   description:
     "The ultimate language toolkit powered by AI. Tranlation, grammar review, and more!",
-  url: "https://langswift.com",
   author: "ricardogzm",
   links: {
     github: "https://github.com/ricardogzm",
