76a3499200f6e82cb1ae7dc6f5d73b927fea76b6
Ricardo Guzman <dev@ricardogzm.com>
2023-09-21 22:47:14 -0600

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/config/site";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: siteConfig.url,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${siteConfig.url}/translate`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+  ];
+}
